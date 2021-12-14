138539ce18f1e1d2f3050d3ee1e55c700b88fac2
Kamran Ali Nejad-Sattary <kamran.nejad-sattary@epfl.ch>
2021-12-14 12:34:01 +0100

    Started writing the code for the project, focussing on preprocessing so far. Applied for API access to intraday stock data, will have GME data on the minute level by later today. See top of notebook for more comments.

---
+++ src/helpers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 14 2021
+@author: Amin Debabeche, Kamran Nejad-Sattary
+Refs
+"""
+
+import os
+import re  
+import pandas as pd  
+from time import time 
+import sys
+
+import spacy  
+import logging  
+import nltk
+import multiprocessing
+
